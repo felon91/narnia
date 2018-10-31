b65f5cd92355f45c4d48b03f883d05664cdfb450
felon91 <felon_91@mail.ru>
2018-10-31 18:04:22 +0300

    edits trello and smooth scroll

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
     margin: 0,
     dots: true,
     nav: true,
+    autoHeight: true,
     loop: true,
     navText: ['<span>След<b>ующий</b></span>', '<span>Пред<b>ыдущий</b></span>'],
     responsive: {
@@ -15,5 +16,27 @@ $(document).ready(function () {
   });
 
   $(".mask").mask("+7 (999) 999-99-99");
+
+  $('.gallery__btn, .gist__btn, .themes__btn').bind("click", function(e){
+    $('html, body').stop().animate({
+      scrollTop: $('#participation').offset().top
+    }, 1000);
+    e.preventDefault();
+  });
+
+  $('.price__btn').bind("click", function(e){
+    $('html, body').stop().animate({
+      scrollTop: $('#meeting').offset().top
+    }, 1000);
+    e.preventDefault();
+  });
+
+  $('.menu__link').bind("click", function(e){
+    var anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $(anchor.attr('href')).offset().top
+    }, 1000);
+    e.preventDefault();
+  });
 
 });
